5bbcb0cdb103cf4575baa6a49c4a53f89d882545
Aleksandr <alexchembarov@gmail.com>
2023-04-18 20:35:35 +0400

    HomeWork

---
+++ HomeWork3/HomeWork3.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE lesson3;
+
+USE lesson3;
+
+CREATE TABLE staff
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(45),
+    last_name VARCHAR(45),
+    post VARCHAR(45),
+    seniority INT,
+    salary INT,
+    age INT
+);
+
+INSERT INTO staff (first_name, last_name, post, seniority, salary, age)
+VALUES 
+ ('Вася', 'Петров', 'Начальник', 40, 100000, 60),
+ ('Петр', 'Власов', 'Начальник', 8, 70000, 30),
+ ('Катя', 'Катина', 'Инженер', 2, 70000, 25),
+ ('Саша', 'Сасин', 'Инженер', 12, 50000, 35),
+ ('Иван', 'Иванов', 'Рабочий', 40, 30000, 59),
+ ('Петр', 'Петров', 'Рабочий', 20, 25000, 40),
+ ('Сидр', 'Сидоров', 'Рабочий', 10, 20000, 35),
+ ('Антон', 'Антонов', 'Рабочий', 8, 19000, 28),
+ ('Юрий', 'Юрков', 'Рабочий', 5, 15000, 25),
+ ('Максим', 'Максимов', 'Рабочий', 2, 11000, 22),
+ ('Юрий', 'Галкин', 'Рабочий', 3, 12000, 24),
+ ('Людмила', 'Маркина', 'Уборщик', 10, 10000, 49);
+ 
+# 1. Отсортируйте данные по полю заработная плата (salary) в порядке: возрастания
+SELECT *
+FROM staff
+ORDER BY salary;
+
+#  Отсортируйте данные по полю заработная плата (salary) в порядке: убывания
+SELECT *
+FROM staff
+ORDER BY salary DESC;
+
+# 2. Выведите 5 максимальных заработных плат (saraly)
+SELECT salary
+FROM staff
+ORDER BY salary DESC
+LIMIT 5;
+
+# 3. Посчитайте суммарную зарплату (salary) по каждой специальности (роst)
+SELECT post as 'специальность', SUM(salary) AS 'суммарная зарплата'
+FROM staff
+GROUP BY post;
+
+# 4. Найдите кол-во сотрудников с специальностью (post) «Рабочий» в возрасте от 24 до 49 лет включительно.
+SELECT COUNT(id) AS count_id
+FROM staff
+WHERE post = "Рабочий" AND age > 24 AND age <= 49;
+
+#check,
+SELECT *
+FROM staff
+WHERE post = "Рабочий";
+
+#5.Найдите количество специальностей
+SELECT COUNT(DISTINCT post)
+FROM staff;
+
+# 6. Выведите специальности, у которых средний возраст сотрудников меньше 30 лет
+SELECT post
+FROM staff
+GROUP BY post
+HAVING AVG(age) < 30;
+# check, средний возраст специальности (сотрудников)
+SELECT post, AVG(age)
+FROM staff
+GROUP BY post;
